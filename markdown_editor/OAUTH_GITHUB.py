edfb4742e9dee15f56147ed84147570cdb1151dc
wangh <wanghan@DESKTOP-3S9VQOF>
2017-03-20 21:42:07 +0800

    xu....

--- markdown_editor/OAUTH_GITHUB.py
+++ markdown_editor/OAUTH_GITHUB.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 CLIENT_ID = 'f0e82131f29cf4177970'
-CLIENT_SECRET = '392de048916c5104e70d88d27e7c056bd9cf30ad'
+CLIENT_SECRET = ''
 URL_ACCESS_TOKEN = 'https://github.com/login/oauth/access_token'
 URL_GET_USER = 'https://api.github.com/user'
 URL_REDIRECT = 'http://59.110.139.171:9876/home'
